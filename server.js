eef864678fd48329abb333f3b480ca34a44894bd
Joe King <jking0186@gmail.com>
2020-06-18 13:59:55 -0500

    data push

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var http = require("http");
 
 var PORT = process.env.PORT || 3000;
 
+var app = express();
+
 // Create a generic function to handle requests and responses
 function handleRequest(request, response) {
   response.end("It Works!! Path Hit: " + request.url);
@@ -14,4 +16,24 @@ var server = http.createServer(handleRequest);
 // Start our server
 server.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
+});
+
+// Data
+
+var reservation = [{
+      name: "Williams",
+      phone: 615-555-1234,
+      email: "williams@email.com",
+      id: 6785
+  }, {
+    name: "Carter",
+    phone: 865-555-6789,
+    email: "carter@email.com",
+    id: 6786
+  }];
+
+
+  // Displays all reservations
+app.get("/", function(req, res) {
+  return res.json(reservations);
 });
